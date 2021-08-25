8f0fca21877881d3cf3520042fa0ea538e3f336a
hwpark <hwpark@malgn.com>
2021-08-25 13:45:00 +0900

    feat : item entity 에 비지니스 로직 추가

--- src/main/java/jpabook/jpbshop/domain/item/Item.java
+++ src/main/java/jpabook/jpbshop/domain/item/Item.java
@@ -1,10 +1,17 @@
 package jpabook.jpbshop.domain.item;
 
-import jpabook.jpbshop.domain.Category;
-
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToMany;
+import jpabook.jpbshop.domain.Category;
+import jpabook.jpbshop.exception.NotEnoughStockException;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -27,39 +34,69 @@ public abstract class Item {
     return id;
   }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
-
   public String getName() {
     return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
   }
 
   public int getPrice() {
     return price;
   }
 
-  public void setPrice(int price) {
-    this.price = price;
-  }
-
   public int getStockQuantity() {
     return stockQuantity;
-  }
-
-  public void setStockQuantity(int stockQuantity) {
-    this.stockQuantity = stockQuantity;
   }
 
   public List<Category> getCategories() {
     return categories;
   }
 
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  public void setStockQuantity(int stockQuantity) {
+    this.stockQuantity = stockQuantity;
+  }
+
   public void setCategories(List<Category> categories) {
     this.categories = categories;
+  }
+
+  //==비지니스 로직 ==//
+  // stockQuantity 는 Item 이 가지고 있기 때문에, stockQuantity 관련 비지니스 로직은 entity 에서 관리하는것이 좋음
+  // setter 로 하는게 data 를 set 하는 것이 아니라 비지니스 메서드를 통해 data 를 control 해야 한다.
+
+  /**
+   * stock 증가
+   *
+   * @param quantity
+   */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   * stock 감소
+   *
+   * @param quantity
+   */
+  public void removeStock(int quantity) {
+
+    // 0 보다 작을 경우 확인
+    int restStock = this.stockQuantity - quantity;
+
+    if (restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+
+    this.stockQuantity -= quantity;
   }
 }
