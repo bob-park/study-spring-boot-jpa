5e6baefe780ba2954fc1a5fd61997274119011f2
hwpark <hwpark@malgn.com>
2021-08-27 15:20:11 +0900

    feat : 상품 주문 조회 및 주문 취소 기능 추가

--- src/main/java/jpabook/jpbshop/controller/OrderController.java
+++ src/main/java/jpabook/jpbshop/controller/OrderController.java
@@ -1,15 +1,15 @@
 package jpabook.jpbshop.controller;
 
 import jpabook.jpbshop.domain.Member;
+import jpabook.jpbshop.domain.Order;
 import jpabook.jpbshop.domain.item.Item;
+import jpabook.jpbshop.repository.OrderSearch;
 import jpabook.jpbshop.service.ItemService;
 import jpabook.jpbshop.service.MemberService;
 import jpabook.jpbshop.service.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,6 +44,21 @@ public class OrderController {
       @RequestParam Long memberId, @RequestParam Long itemId, @RequestParam int count) {
     orderService.order(memberId, itemId, count);
 
+    return "redirect:/orders";
+  }
+
+  @GetMapping("/orders")
+  public String orderList(@ModelAttribute OrderSearch orderSearch, Model model) {
+    List<Order> orders = orderService.findOrders(orderSearch);
+
+    model.addAttribute("orders", orders);
+
+    return "order/orderList";
+  }
+
+  @PostMapping("/orders/{orderId}/cancel")
+  public String cancelOrder(@PathVariable Long orderId) {
+    orderService.cancelOrder(orderId);
     return "redirect:/orders";
   }
 }
