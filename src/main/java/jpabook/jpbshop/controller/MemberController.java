7896ee40d60a752aefa478dbbaf8b81276a23acd
hwpark <hwpark@malgn.com>
2021-08-27 11:34:36 +0900

    feat : 회원가입 페이지 추가

---
+++ src/main/java/jpabook/jpbshop/controller/MemberController.java
@@ -0,0 +1,24 @@
+package jpabook.jpbshop.controller;
+
+import jpabook.jpbshop.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "members/createMemberForm";
+    }
+}
