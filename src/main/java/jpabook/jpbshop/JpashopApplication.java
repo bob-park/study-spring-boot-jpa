4daf847eeeb33e282ef9412ba3ee3efc68fff335
phw11 <hwpark11@kakao.com>
2021-08-29 15:31:06 +0900

    feat : hibernate module bean 등록 추가

--- src/main/java/jpabook/jpbshop/JpashopApplication.java
+++ src/main/java/jpabook/jpbshop/JpashopApplication.java
@@ -1,12 +1,23 @@
 package jpabook.jpbshop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(JpashopApplication.class, args);
+  }
+
+  @Bean
+  public Hibernate5Module getHibernate5Module() {
+    Hibernate5Module module = new Hibernate5Module();
+
+    //    module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+
+    return module;
   }
 }
